ec1d7ea1c67c18a5a045fa9c4e355e0cf70f17ca
rom click <xxopk11@126.com>
2025-12-12 18:21:06 +0800

    Rom commit

--- contracts/router/FlashLoanRouter.sol
+++ contracts/router/FlashLoanRouter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "../interface/IFlashLoan.sol";
+import "../interfaces/IFlashLoan.sol";
 import "../core/ConfigManage.sol";
 
 contract FlashLoanRouter {
