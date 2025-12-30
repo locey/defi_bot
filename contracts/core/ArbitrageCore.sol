4ac3f8f84dfa994687cf725acbf6561cbb9d1963
rom click <xxopk11@126.com>
2025-12-30 10:01:17 +0800

    Rom commit paused

--- contracts/core/ArbitrageCore.sol
+++ contracts/core/ArbitrageCore.sol
@@ -34,7 +34,7 @@ contract ArbitrageCore is Initializable, UUPSUpgradeable, OwnableUpgradeable, Re
     address public platFormWallet;
     address public backCaller;              //后端
     address[] public supportAssets;
-
+    bool public paused;
 
     //金库地址
     mapping(address => IArbitrageVault) public vaults;
@@ -63,6 +63,11 @@ contract ArbitrageCore is Initializable, UUPSUpgradeable, OwnableUpgradeable, Re
 
     modifier onlybackCaller() {
         require(msg.sender == backCaller || msg.sender == owner(), "Only back or owner can call");
+        _;
+    }
+
+    modifier whenNotPaused() {
+        require(!paused, "Arbitrage Contract is paused");
         _;
     }
 
@@ -128,6 +133,10 @@ contract ArbitrageCore is Initializable, UUPSUpgradeable, OwnableUpgradeable, Re
         );
     }
 
+    function setPaused(bool _paused) external onlyOwner {
+        paused = _paused;
+    }
+
     //核心函数：实现套利的调用，并对盈利&分润计算
     /**
     *入参：
@@ -150,7 +159,7 @@ contract ArbitrageCore is Initializable, UUPSUpgradeable, OwnableUpgradeable, Re
     function executeStrategy(
         StrategyTypes strategyTypes,
         IArbitrage.ArbitrageParams calldata params
-    ) external nonReentrant onlybackCaller {
+    ) external nonReentrant whenNotPaused onlybackCaller {
         if (strategyTypes == StrategyTypes.OWN_FUNDS ) {
             _executedVaultArbitrage(params);
         } else if (strategyTypes == StrategyTypes.FLASH_LOAN) {
