c92e947d41f8f40b86f541b1a629abef8e6d7650
adamli <adamliy488@gmail.com>
2026-01-16 03:42:07 +0800

    feat(contract): update smart contracts and add Arbitrum deployment

--- contract/contracts/integrations/UniswapV2V3Integration.sol
+++ contract/contracts/integrations/UniswapV2V3Integration.sol
@@ -6,7 +6,7 @@ import "../router/IUniswapV2Router02.sol";
 import "../router/IUniswapV3QuoterV2.sol";
 import "../router/IUniswapV3Router.sol";
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
